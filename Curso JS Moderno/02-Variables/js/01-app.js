0e005e5c7c43aa3a8c474572700764fa47debda6
Luis Daniel <gonzalez.zamudio.luis.daniel@gmail.com>
2023-03-09 14:06:03 -0600

    feat: se avanzó en el curso hasta el video 13

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,18 @@
+var producto = "Monitor de 24  Pulgadas";
+console.log(producto)
+producto = 'Monitor de 19 pulgadas';
+console.log(producto)
+producto = 20;
+console.log(producto)
+
+var disponible;
+disponible = true;
+
+disponible = false;
+
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+var 99dias;
+var dias99;
