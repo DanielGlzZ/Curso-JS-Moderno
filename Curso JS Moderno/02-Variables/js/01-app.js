8284b36b93adcc69c87166a96cfbfaf7e38d712d
Luis Daniel <gonzalez.zamudio.luis.daniel@gmail.com>
2023-03-10 10:11:43 -0600

    feat: continuación del video de varibles

--- Curso JS Moderno/02-Variables/js/01-app.js
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -14,5 +14,14 @@ var categoria = 'Computadoras',
     marca = "Marca Famosa",
     calificacion = 5;
 
-var 99dias;
+// var 99dias;
 var dias99;
+
+// var 01_;
+var _01;
+var _usuario;
+var __usuario;
+
+var nombreProducto;
+var nombre_producto_categoria_precio;
+var NombreProducto;
