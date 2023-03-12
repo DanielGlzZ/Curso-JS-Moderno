5278b8a0fb94f21f98bb158322ded135f80a3c73
Luis Daniel <gonzalez.zamudio.luis.daniel@gmail.com>
2023-03-11 21:15:44 -0600

    feat: Se avanzó hasta el video 40 del curso

--- Curso JS Moderno/04-Numeros/js/04-app.js
+++ Curso JS Moderno/04-Numeros/js/04-app.js
@@ -4,4 +4,9 @@ resultado = 20 + 30 * 2;
 
 resulatdo = ( 20 + 30 ) * 2;
 
+// 20% DE DESCUENTO EN TU CARRITO DE COMPRAS
+resultado = (20 + 30 + 40 + 50 + 60) * .2;
+
+resultado = (20, + 30) * 1.16;
+
 console.log(resultado);
---
+++ Curso JS Moderno/07-Objetos/js/04-app.js
@@ -0,0 +1,17 @@
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+// const nombre = producto.nombre;
+
+// console.log(nombre);
+
+// Destructuring
+
+const { nombre, precio, disponible, noExiste } = producto;
+console.log(nombre);
+console.log(precio);
+console.log(disponible);
+console.log(noExiste);
