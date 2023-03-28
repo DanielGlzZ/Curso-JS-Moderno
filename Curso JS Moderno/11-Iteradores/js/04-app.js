3648c5f7c889d90bcef7e776153359b08f1b27d4
Luis Daniel <gonzalez.zamudio.luis.daniel@gmail.com>
2023-03-27 18:08:59 -0600

    feat: se avanzó hasta el video 85

---
+++ Curso JS Moderno/11-Iteradores/js/04-app.js
@@ -0,0 +1,12 @@
+let i = 1; //Incializar el While
+
+while( i < 100 ) { //Condición
+
+    if(i % 2 === 0) {
+        console.log(`El número es par ${i}`);
+    } else {
+        console.log(`El número es impar ${i}`);
+    }
+
+    i++; //Incremento
+}
---
+++ Curso JS Moderno/11-Iteradores/js/05-app.js
@@ -0,0 +1,13 @@
+let i = 1; //Incializar el While
+
+do {
+    if(i % 2 === 0) {
+        console.log(`El número es par ${i}`);
+    } else {
+        console.log(`El número es impar ${i}`);
+    }
+    i++; //Incremento
+}
+while( i < 100 ); //Condición
+    
+
